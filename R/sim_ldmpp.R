080ff035016eabf42beced8e445adf73906af3ae
Lane Drew <lanetdrew@gmail.com>
2024-06-26 15:20:51 -0600

    Changed subsetting

--- R/sim_ldmpp.R
+++ R/sim_ldmpp.R
@@ -102,8 +102,8 @@ pred_marks <- function(sim_realization, raster_list, size_model){
 #'
 generate_mpp <- function(location_data, marks, window){
 
-  marked_pp <- spatstat.geom::ppp(location_data[,2],
-                                  location_data[,3],
+  marked_pp <- spatstat.geom::ppp(location_data[,c("x")],
+                                  location_data[,c("y")],
                                   window[1:2], window[3:4],
                                   marks = marks)
 
