ea145c9c0bf9941e922b4345bc5f1441fd7f8f82
Lane Drew <lanetdrew@gmail.com>
2024-06-26 15:46:35 -0600

    demo updates.

--- R/plot_sim.R
+++ R/plot_sim.R
@@ -9,7 +9,7 @@ plot_sim <- function(sim_data){
   sim_plot <- base::as.data.frame(sim_data) |>
     ggplot2::ggplot(ggplot2::aes(x = sim_data$x, y = sim_data$y, size = sim_data$marks)) +
     ggplot2::geom_point(alpha = .5) +
-    ggplot2::labs(x = "", y = "", title = "Simulated Data") +
+    ggplot2::labs(x = "", y = "", title = "Simulated Data", size = "Mark") +
     ggplot2::theme_bw()
   return(sim_plot)
 }
@@ -25,7 +25,7 @@ plot_refdata <- function(ref_data){
   ref_plot <- base::as.data.frame(ref_data) |>
     ggplot2::ggplot(ggplot2::aes(x = ref_data$x, y = ref_data$y, size = ref_data$marks)) +
     ggplot2::geom_point(alpha = .5) +
-    ggplot2::labs(x = "", y = "", title = "Reference Data") +
+    ggplot2::labs(x = "", y = "", title = "Reference Data", size = "Mark") +
     ggplot2::theme_bw()
   return(ref_plot)
 }
--- R/sim_ldmpp.R
+++ R/sim_ldmpp.R
@@ -104,6 +104,7 @@ pred_marks <- function(sim_realization, raster_list, size_model){
 #'
 generate_mpp <- function(location_data, marks, window){
 
+  location_data <- base::as.data.frame(location_data)
   marked_pp <- spatstat.geom::ppp(location_data[,c("x")],
                                   location_data[,c("y")],
                                   window[1:2], window[3:4],
