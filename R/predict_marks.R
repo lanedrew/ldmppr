72e8fe95fb99d9354d7ae4c95257947f638dfc23
Lane Drew <lanetdrew@gmail.com>
2024-07-30 17:23:27 -0600

    Typos.

--- R/predict_marks.R
+++ R/predict_marks.R
@@ -19,26 +19,26 @@ predict_marks <- function(sim_realization, raster_list, size_model){
   X$near.nbr.dist <- NA
   X$near.nbr.num <- NA
   X$avg.nbr.dist.15 <- NA
-  X$near.nbr.size <- NA
-  X$near.nbr.size.all <- NA
-  X$near.nbr.size.dist.ratio <- NA
+  X$near.nbr.time <- NA
+  X$near.nbr.time.all <- NA
+  X$near.nbr.time.dist.ratio <- NA
   distance.matrix <- base::as.matrix(stats::dist(s, method = "euclidean"))
 
   for(i in 1:base::nrow(X)){
     close.points.15 <- base::unique(base::which(distance.matrix[i,] < 15 & distance.matrix[i,] != 0))
     close.times.15 <- X$time[close.points.15]
-    X$near.nbr.dist[i] <- min(distance.matrix[i,][-i])
-    X$near.nbr.num[i] <- length(close.points.15)
-    X$avg.nbr.dist.15[i] <- mean(distance.matrix[i,][close.points.15])
-    if(length(close.points.15) == 0){
-      X$avg.nbr.dist.15[i] <- min(distance.matrix[i,][-i])
+    X$near.nbr.dist[i] <- base::min(distance.matrix[i,][-i])
+    X$near.nbr.num[i] <- base::length(close.points.15)
+    X$avg.nbr.dist.15[i] <- base::mean(distance.matrix[i,][close.points.15])
+    if(base::length(close.points.15) == 0){
+      X$avg.nbr.dist.15[i] <- base::min(distance.matrix[i,][-i])
     }
     X$near.nbr.time[i] <- X$time[base::unique(base::which(distance.matrix[i,] == X$near.nbr.dist[i]))]
     X$near.nbr.time.all[i] <- mean(close.times.15)
-    if(length(close.points.15) == 0){
+    if(base::length(close.points.15) == 0){
       X$near.nbr.time.all[i] <- X$time[base::unique(base::which(distance.matrix[i,] == X$near.nbr.dist[i]))]
     }
-    X$near.nbr.size.dist.ratio[i] <- X$near.nbr.time[i]/X$near.nbr.dist[i]
+    X$near.nbr.time.dist.ratio[i] <- X$near.nbr.time[i]/X$near.nbr.dist[i]
   }
 
   return(stats::predict(size_model, X))
