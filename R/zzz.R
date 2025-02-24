882001f587d0a59d85aef6ac7b05f992bac96fe7
Lane Drew <lanetdrew@gmail.com>
2025-02-24 14:03:37 -0700

    Updated .onLoad to deal with multithreading behavior.

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,10 @@
 # This file contains the .onLoad() function to handle initialization tasks
 # and ensure imports from xgboost and ranger are recognized.
 .onLoad <- function(libname, pkgname) {
+
+  # CRAN OMP THREAD LIMIT
+  Sys.setenv("OMP_THREAD_LIMIT" = 1)
+
   # Reference the functions to ensure they are recognized
   xgboost::xgboost
   ranger::ranger
